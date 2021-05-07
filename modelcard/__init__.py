84331bd06454680424210d4f55e638a15cb9fe66
adrinjalali <adrin.jalali@gmail.com>
2021-05-07 16:15:49 +0200

    Add ModelCard object definition and schema

--- modelcard/__init__.py
+++ modelcard/__init__.py
@@ -1,1 +1,5 @@
+from ._model_card import ModelCard
+
 __version__ = "0.1"
+
+__all__ = ["ModelCard"]
---
+++ modelcard/utils/__init__.py
@@ -0,0 +1,4 @@
+from ._validation import validate_json_schema
+from ._validation import get_latest_schema_version
+
+__all__ = ["validate_json_schema", "get_latest_schema_version"]
